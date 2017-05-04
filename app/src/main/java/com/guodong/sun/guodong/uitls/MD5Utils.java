4174a3c23708e27cd43f462413d662e10e55e300
孙国栋 <sun33919135@gmail.com>
2017-05-04 15:11:54 +0800

    Add Copyright to each file

--- app/src/main/java/com/guodong/sun/guodong/uitls/MD5Utils.java
+++ app/src/main/java/com/guodong/sun/guodong/uitls/MD5Utils.java
@@ -1,6 +1,29 @@
+/*
+ * Copyright (C) 2017 guodongAndroid
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Last modified 2017-05-04 15:05:52
+ *
+ * GitHub:   https://github.com/guodongAndroid
+ * Website:  http://www.sunxiaoduo.com
+ * Email:    sun33919135@gmail.com
+ * QQ:       33919135
+ */
+
 package com.guodong.sun.guodong.uitls;
 
-import android.support.v4.app.NavUtils;
+import android.support.annotation.NonNull;
 
 import java.security.MessageDigest;
 
@@ -14,7 +37,7 @@ public class MD5Utils {
      * @param message
      * @return
      */
-    public static String getMD5(String message) {
+    public static String getMD5(@NonNull String message) {
         String md5str = null;
         try {
             //1 创建一个提供信息摘要算法的对象，初始化为md5算法对象
